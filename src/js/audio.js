9ef497f4a5197a98d9f1c5d2eba48d74393ed469
droom <chrisdroom@gmail.com>
2016-03-28 15:26:00 +0100

    v3

---
+++ src/js/audio.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+
+
+  // var beep = $("#beep")[0];
+  // $("a.plain")
+  // .mouseenter(function() {
+  //   beep.pause();
+  //   beep.play();
+  //   console.log("hi")
+  // });
+  // $( "a" ).click(function(event ) {
+  //   $('body').fadeOut(150);
+  // });
+
+
+
+});
---
+++ src/js/storage.js
@@ -0,0 +1,16 @@
+// $(document).ready(function() {
+
+// console.log($(".worklist"));
+
+// console.log("hello");
+
+// var supported = true;
+// for (var feature in Modernizr) {
+//   if (typeof Modernizr[feature] === "boolean" && Modernizr[feature] == false) {
+//     supported = false;
+//     break;
+//   }
+// }
+
+
+// });
