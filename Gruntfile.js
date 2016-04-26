b2c7211ead3ef6a1fd70d118955618105b82cc74
droom <chrisdroom@gmail.com>
2016-04-26 13:16:47 +0100

    changes watch partial path

--- Gruntfile.js
+++ Gruntfile.js
@@ -19,7 +19,7 @@ module.exports = function(grunt) {
       },
 
       jade: {
-        files: ['src/jade/*.jade', 'src/jade/partials/*.jade'],
+        files: ['src/jade/*.jade', 'src/jade/projects/*.jade'],
         tasks: ['jade'],
       },
 
